f515d2cb8045fedd9f0c4720c525d8c72accdeaa
romain <romain.tholle@ynov.com>
2026-04-11 15:39:44 +0200

    modif Jeu

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,6 @@ import javax.swing.SwingUtilities;
 
 public class Main {
     public static void main(String[] args) {
-        // Lance le jeu de manière sécurisée
-        SwingUtilities.invokeLater(() -> new Jeu());
+        SwingUtilities.invokeLater(Jeu::new);
     }
 }
